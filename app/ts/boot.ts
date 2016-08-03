e13ad2daef5eb0135a8d8c187d665b186be3a374
Angel <mendezz.angel@gmail.com>
2016-08-03 10:49:27 -0500

    Removed tons of node_modules

--- app/ts/boot.ts
+++ app/ts/boot.ts
@@ -1,9 +1,9 @@
 import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
-import {ServicesComponent} from './services.component'
-import {PortfolioComponent} from './portfolio.component'
-import {MainHeaderComponent} from "./mainHeader.component";
-import {MainNavComponent} from "./mainNav.component";
-import {PortfolioModalsComponent} from "./portfolioModals.component";
+import {AppComponent} from './components/app.component'
+import {ServicesComponent} from './components/services.component'
+import {PortfolioComponent} from './components/portfolio.component'
+import {MainHeaderComponent} from "./components/mainHeader.component";
+import {MainNavComponent} from "./components/mainNav.component";
+import {PortfolioModalsComponent} from "./components/portfolioModals.component";
 
 bootstrap(AppComponent);
---
+++ app/ts/services/avellaxService.ts
@@ -0,0 +1,8 @@
+﻿import {Injectable} from "angular2/core";
+
+@Injectable()
+export class AvellaxService {
+    getHeaderImage() {
+        
+    }
+}
